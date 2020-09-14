896c1d11b8552f3cb33e1c1024f4ec45a682bf30
Juicya <juicya14@gmail.com>
2020-09-14 12:12:28 +0700

    none

---
+++ project_2.py
@@ -0,0 +1,35 @@
+import tempfile
+import os
+import argparse
+import json
+
+def function():
+    """Working with Python Console."""
+    objects_all = argparse.ArgumentParser()         # создан объект;
+    objects_all.add_argument('--key', type=str, help = 'Return key\'s dictionary')        # описание переменной, которую планируется запсиать
+    objects_all.add_argument('--value',type=str, help = 'Return values\'s dictionary')
+
+    arguments = objects_all.parse_args()            # то, что мы предали станет свойствами объекта arg
+    if arguments.value is None:
+        pass
+    elif arguments.key is None:
+        print('Error, key can not be None!')
+
+    __key = arguments.key
+    __value = arguments.value
+    function_0(__key, __value)
+
+function()
+
+
+def function_0(key, value, name_of_file='storage.data'):
+    """The function for recording information in time-file."""
+    dict_1 = dict()
+    dict_2 = dict()
+    storage_path = os.path.join(tempfile.gettempdir(), name_of_file)
+
+    with open(storage_path, 'w+') as name_of_file:
+        mean = {'keys :': list(),'value :':dict()}
+        json.dump(mean, name_of_file)
+
+

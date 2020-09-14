db1e2331f7560ff355484ee42267838de4d9da75
Juicya <juicya14@gmail.com>
2020-09-14 12:20:42 +0700

    none_2

--- project_2.py
+++ project_2.py
@@ -3,13 +3,15 @@ import os
 import argparse
 import json
 
+
 def function():
     """Working with Python Console."""
     objects_all = argparse.ArgumentParser()         # создан объект;
     objects_all.add_argument('--key', type=str, help = 'Return key\'s dictionary')        # описание переменной, которую планируется запсиать
     objects_all.add_argument('--value',type=str, help = 'Return values\'s dictionary')
 
-    arguments = objects_all.parse_args()            # то, что мы предали станет свойствами объекта arg
+    arguments = objects_all.parse_args()            # то, что мы предали(key, value) станет свойствами объекта arg
+
     if arguments.value is None:
         pass
     elif arguments.key is None:
@@ -19,13 +21,11 @@ def function():
     __value = arguments.value
     function_0(__key, __value)
 
-function()
 
 
 def function_0(key, value, name_of_file='storage.data'):
     """The function for recording information in time-file."""
-    dict_1 = dict()
-    dict_2 = dict()
+
     storage_path = os.path.join(tempfile.gettempdir(), name_of_file)
 
     with open(storage_path, 'w+') as name_of_file:
@@ -33,3 +33,4 @@ def function_0(key, value, name_of_file='storage.data'):
         json.dump(mean, name_of_file)
 
 
+function()
